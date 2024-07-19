867cd2988d472c16621f9fa316c4205d0f11f076
XML <13480465265@126.com>
2024-07-19 21:40:41 +0800

    Initial commit

---
+++ harA/hvigorfile.ts
@@ -0,0 +1,11 @@
+import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { routerRegisterPlugin, PluginConfig } from 'router-register-plugin'
+
+const config: PluginConfig = {
+  scanDir: "src/main/ets/components",
+  logEnabled: false,
+}
+export default {
+    system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[routerRegisterPlugin(config)]         /* Custom plugin to extend the functionality of Hvigor. */
+}
