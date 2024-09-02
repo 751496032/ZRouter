dafa4e201d6d690f8e8ea6c631436902c77b15e3
XML <13480465265@126.com>
2024-09-03 02:27:01 +0800

    插件1.0.5

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,9 +1,10 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 import { routerRegisterPlugin, PluginConfig } from 'router-register-plugin'
 const config: PluginConfig = {
-    scanDir: "src/main/ets/pages",
+    scanDir: 'src/main/ets/pages',
     logEnabled: false,
     viewNodeInfo: false,
+
 }
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
