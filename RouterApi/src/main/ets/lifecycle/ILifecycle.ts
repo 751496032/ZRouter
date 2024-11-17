323b08f4344dcd3ab00cbd4702fb909275dbf606
XML <13480465265@126.com>
2024-11-17 23:58:23 +0800

    template test

--- RouterApi/src/main/ets/lifecycle/ILifecycle.ts
+++ RouterApi/src/main/ets/lifecycle/ILifecycle.ts
@@ -8,7 +8,7 @@ import { RouterInfo } from "../model/RouterInfo"
 
 export interface ILifecycle {}
 
-export interface IPageLifeCycle extends ILifecycle {
+export interface IPageLifecycle extends ILifecycle {
   pageClasName?: string
 
   aboutToAppear?(): void
@@ -33,7 +33,7 @@ export interface IPageLifeCycle extends ILifecycle {
 
 }
 
-export interface INavLifeCycle extends ILifecycle {
+export interface INavLifecycle extends ILifecycle {
   /**
    * 当该NavDestination页面显示时触发此回调
    */
@@ -86,4 +86,17 @@ export interface INavLifeCycle extends ILifecycle {
 
 }
 
-// export type RouterFunc = (info?: RouterInfo) => void
+export interface INavActiveLifecycle extends ILifecycle {
+
+  onWillShow?(info: RouterInfo): void
+
+  onShown?(info: RouterInfo): void
+
+  onWillHide?(info: RouterInfo): void
+
+  onHidden?(info: RouterInfo): void
+
+  onWillDisappear?(info: RouterInfo): void
+
+  onDisappear?(info: RouterInfo): void
+}
